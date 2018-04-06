bcd5872b2fa8bd4e115bc6b83e74403d2a504e6d
Nathan Johnson <nathancj224@gmail.com>
2018-04-06 19:58:56 -0400

    Attendant screen deisgn mainly done, lots of progress

--- js/attendant.js
+++ js/attendant.js
@@ -1,7 +1,48 @@
+Vue.component('item-table', {
+    template: '#item-table',
+    data: function() {
+        return {
+            items: [
+                {
+                    name: '54x40 Matte Poster',
+                    qty: 1,
+                    cost: 27
+                }
+            ]
+        };
+    },
+    computed: {
+        parsedItems: function() {
+            if(!this.items) return;
+            if(this.items.length <= 0)
+                this.items.push({ name: '...', cost: 0, qty: 0 });
+            return this.items.map((item) => { 
+                return {
+                    name: item.name,
+                    cost: item.cost ? '$' + this.moneyFormat(item.cost) : '...',
+                    qty: item.qty ? item.qty : '...',
+                    total: item.cost ? '$' + this.moneyFormat(item.cost*item.qty) : '...'
+                };
+            });
+        }
+    },
+    methods: {
+        moneyFormat: function(num) {
+            return num % 1 === 0 ? num : num.toFixed(2);
+        },
+    }
+});
+
 let app = new Vue({
     el: '#app',
     data: {
-        isSignedIn: false
+        currentOrder: null,
+        isSignedIn: true,
+        isCurrentOrder: false,
+        isCurrentOrderComplete: false
+    },
+    mounted: function() {
+
     },
     methods: {
         signIn: function() {
